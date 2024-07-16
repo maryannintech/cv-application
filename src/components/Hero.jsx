5cabd7b0eb052d4fd6da533fc23cbaa172dfcd43
mary ann <114210058+maryannintech@users.noreply.github.com>
2024-07-16 16:44:30 +0800

    delete not used files (app.jsx and css)

---
+++ src/components/Hero.jsx
@@ -0,0 +1,10 @@
+import './Hero.css';
+
+export default function Hero(){
+    return (
+        <div className='title'>
+            <h1>CV Builder</h1>
+            <h2>Design your CV from scratch</h2>
+        </div>
+    )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import Hero from './components/Hero.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <Hero></Hero>
+  </React.StrictMode>
 )
