abe83a8e7b719f7fbc6a8d2aaa450555af2c353a
mary ann <114210058+maryannintech@users.noreply.github.com>
2024-07-18 10:48:52 +0800

    changes

---
+++ src/components/PracticalExperience.jsx
@@ -0,0 +1,14 @@
+import "../css/Fonts.css";
+import "../css/PracticalExperience.css";
+
+export default function PracticalExperience() {
+    return (
+        <div className="form">
+            <form>
+                <div className="practicalinfo">
+                    
+                </div>
+            </form>
+        </div>
+    )
+}
